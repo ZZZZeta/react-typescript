40eb3b737cf1ee191ed3c68088f5037bc5131e9f
Yan Postnov <y.postnov@helsi.me>
2020-12-11 09:44:22 +0200

    refactor setupProxy to typescript

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,13 +1,17 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import { createProxyMiddleware, Options, RequestHandler } from "http-proxy-middleware";
 
-module.exports = function (app) {
+module.exports = function (app: { use: (arg0: RequestHandler) => void; }) {
   Object.keys(PROXY_SETTINGS).forEach((env) => {
     const options = PROXY_SETTINGS[env];
     app.use(createProxyMiddleware(env, options));
   });
 };
 
-const PROXY_SETTINGS = {
+type ProxySettings = {
+  [url: string]: Options
+}
+
+const PROXY_SETTINGS: ProxySettings = {
   '/api': {
     target: 'http://localhost:5000',
     changeOrigin: true,
